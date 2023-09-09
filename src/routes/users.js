20f71e2942577da5d3d39d5c1fcfabd11c356e4b
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-09-09 20:52:30 +0100

    clean up old structure

--- src/routes/users.js
+++
@@ -1,8 +0,0 @@
-const usersController = require('../controllers/users');
-const router = require('express').Router();
-
-
-router.get('/', usersController.getUsers);
-router.get('/:id', usersController.getUserById);
-
-module.exports = router;
