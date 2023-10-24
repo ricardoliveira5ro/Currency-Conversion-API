c9d13045c675a83626fdcfc08b9855f682d5adb9
Ricardo Oliveira <201901561@estudantes.ips.pt>
2023-10-24 16:26:39 +0100

    enable 'Access-Control-Allow-Origin'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+const cors = require('cors');
 
 const logger = require('./middleware/logger')
 const globalErrorHandler = require('./middleware/globalErrorHandler')
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
